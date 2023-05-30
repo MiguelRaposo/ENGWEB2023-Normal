dd84e910ed6aa4b0d75080c504b62d02617bf005
MiguelRaposo <73466553+MiguelRaposo@users.noreply.github.com>
2023-05-30 16:07:55 +0100

    Update plantas.js

--- ex1/models/plantas.js
+++ ex1/models/plantas.js
@@ -2,23 +2,23 @@ var mongoose = require('mongoose');
 
 
 var plantasSchema = new mongoose.Schema({
-        _id: Number,
-        numero_de_registo: Number,
-        codigo_de_rua: Number,
-        rua: String,
-        local: String,
-        freguesia: String,
-        especie: String,
-        nome_cientifico: String,
-        origem: String,
-        data_de_plantação: String,
-        estado: String,
-        caldeira: String,
-        tutor: String,
-        implantacao: String,
-        gestor: String,
-        data_de_actualização: String,
-        numero_de_intervencoes: Number
+    "_id": Number,
+    "Número de Registo": Number,
+    "Código de rua": Number,
+    "Rua": String,
+    "Local": String,
+    "Freguesia": String,
+    "Espécie": String,
+    "Nome Científico": String,
+    "Origem": String,
+    "Data de Plantação": String,
+    "Estado": String,
+    "Caldeira": String,
+    "Tutor": String,
+    "Implantação": String,
+    "Gestor": String,
+    "Data de actualização": String,
+    "Número de intervenções": Number
 });
 
 
